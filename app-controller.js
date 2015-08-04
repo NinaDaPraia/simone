ac8a8c04b6b1f614b903d7cf6492d936c1eda497
Roselma Mendes <roselma.mendes@gmail.com>
2015-08-04 09:22:04 -0300

    Start code for get Influential Figures.

---
+++ app-controller.js
@@ -0,0 +1,8 @@
+angular.module('SimoneApp')
+
+.controller('SimoneController', function($scope){
+            $scope.figures = [{id: '1',
+                    name: 'Bob Marley',
+                    description: 'Since his passing on May 11, 1981, Bob Marley’s legend looms larger than ever, as evidenced by an ever-lengthening list of accomplishments attributable to his music, which identified oppressors and agitated for social change while simultaneously allowing listeners to forget their troubles and dance.',
+                    socialMovement: 'Black Power'}];
+});
--- app-controller_test.js
+++ app-controller_test.js
@@ -1,5 +1,24 @@
 describe('SimoneApp', function(){
-    it('first test', function(){
-        expect(true).toEqual(true);
+    
+    beforeEach(module('SimoneApp'));
+    
+    var $controller;
+    
+    beforeEach(inject(function(_$controller_){
+        $controller = _$controller_;
+    }));
+    
+    describe('List of Influential Figures', function(){
+        it('Get all Influential Figures in figures variable', function(){
+            var $scope = {};
+            $controller('SimoneController', {$scope: $scope});
+            
+            expect([{id: '1',
+                    name: 'Bob Marley',
+                    description: 'Since his passing on May 11, 1981, Bob Marley’s legend looms larger than ever, as evidenced by an ever-lengthening list of accomplishments attributable to his music, which identified oppressors and agitated for social change while simultaneously allowing listeners to forget their troubles and dance.',
+                    socialMovement: 'Black Power'}])
+            .toEqual($scope.figures);
+        });
     });
+    
 });
